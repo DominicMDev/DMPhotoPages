c4f0d03a1c2ec4fe26d9a1248148a75bcd05e4a6
Dominic Miller <DominicMDev@gmail.com>
2018-10-10 11:23:24 -0700

    Initial Commit

---
+++ DMPhotoPages/DMPhotoPages.h
@@ -0,0 +1,19 @@
+//
+//  DMPhotoPages.h
+//  DMPhotoPages
+//
+//  Created by Dominic Miller on 10/10/18.
+//  Copyright © 2018 DominicMDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DMPhotoPages.
+FOUNDATION_EXPORT double DMPhotoPagesVersionNumber;
+
+//! Project version string for DMPhotoPages.
+FOUNDATION_EXPORT const unsigned char DMPhotoPagesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DMPhotoPages/PublicHeader.h>
+
+
